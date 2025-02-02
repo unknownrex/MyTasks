31778f8c3e2cbdfffbec72c0412b43f584d2569e
Rex <reyvan.haltec@gmail.com>
2025-02-02 19:14:49 +0700

    create MyTasksDatabase

---
+++ data/src/main/java/com/rexample/mytasks/data/database/MyTasksDatabase.kt
@@ -0,0 +1,36 @@
+package com.rexample.mytasks.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.rexample.mytasks.data.dao.CategoryDao
+import com.rexample.mytasks.data.dao.TaskDao
+import com.rexample.mytasks.data.dao.UserDao
+import com.rexample.mytasks.data.entity.CategoryEntity
+import com.rexample.mytasks.data.entity.TaskEntity
+import com.rexample.mytasks.data.entity.UserEntity
+
+@Database(entities = [UserEntity::class, CategoryEntity::class, TaskEntity::class], version = 1, exportSchema = false)
+abstract class MyTasksDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun categoryDao(): CategoryDao
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MyTasksDatabase? = null
+
+        fun getDatabase(context: Context): MyTasksDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MyTasksDatabase::class.java,
+                    "mytasks_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
