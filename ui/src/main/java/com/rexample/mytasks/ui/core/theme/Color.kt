4ff285f504e6a2e6aeb4e9f453cadaba3c130df3
Rex <reyvan.haltec@gmail.com>
2025-02-07 22:59:22 +0700

    Update color

---
+++ ui/src/main/java/com/rexample/mytasks/ui/core/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.rexample.mytasks.ui.core.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primary = Color(0xFF429DD0)
+val secondary = Color(0xFFCBE5FD)
+val white = Color(0xFFF6FCFF)
+val error = Color(0xFFDA3131)
+val black = Color(0xFF485155)
+val grey = Color(0xFF879093)
+val secondaryWhite = Color(0xFFEFFAFF)
