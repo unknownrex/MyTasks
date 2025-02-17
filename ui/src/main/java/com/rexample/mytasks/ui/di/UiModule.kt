1bdff9c6ec3c3a8cd95f02962801e590e57719ec
Rex <reyvan.haltec@gmail.com>
2025-02-17 07:23:11 +0700

    update ui Module

--- ui/src/main/java/com/rexample/mytasks/ui/di/UiModule.kt
+++ ui/src/main/java/com/rexample/mytasks/ui/di/UiModule.kt
@@ -1,6 +1,11 @@
 package com.rexample.mytasks.ui.di
 
+import com.rexample.mytasks.ui.addnewtask.NewTaskViewModel
+import com.rexample.mytasks.ui.edittask.EditTaskViewModel
+import com.rexample.mytasks.ui.home.HomeViewModel
 import com.rexample.mytasks.ui.login.LoginViewModel
+import com.rexample.mytasks.ui.managecategory.ManageCategoryViewModel
+import com.rexample.mytasks.ui.profile.ProfileViewModel
 import com.rexample.mytasks.ui.register.RegisterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,4 +13,9 @@ import org.koin.dsl.module
 val uiModule = module {
     viewModel{ RegisterViewModel(get()) }
     viewModel{ LoginViewModel(get()) }
+    viewModel{ HomeViewModel(get(), get(), get()) }
+    viewModel{ NewTaskViewModel(get(), get(), get()) }
+    viewModel { ManageCategoryViewModel(get(), get()) }
+    viewModel { ProfileViewModel(get(), get(), get()) }
+    viewModel { EditTaskViewModel(get(), get(), get()) }
 }
