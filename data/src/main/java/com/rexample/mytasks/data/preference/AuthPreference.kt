381475d37bd1be18d55e9ee947e4a5ae8f0e537c
Rex <reyvan.haltec@gmail.com>
2025-02-17 07:12:29 +0700

    update user preference

--- data/src/main/java/com/rexample/mytasks/data/preference/AuthPreference.kt
+++ data/src/main/java/com/rexample/mytasks/data/preference/AuthPreference.kt
@@ -1,5 +1,6 @@
 package com.rexample.mytasks.data.preference
 
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
@@ -12,36 +13,27 @@ class AuthPreference(private val dataStore: DataStore<Preferences>) {
 
     companion object {
         private val USER_ID = intPreferencesKey("user_id")
-        private val EMAIL_KEY = stringPreferencesKey("email")
     }
 
-    suspend fun saveUser(userId: Int, email: String) {
+    suspend fun saveUser(userId: Int) {
         dataStore.edit { preferences ->
             preferences[USER_ID] = userId
-            preferences[EMAIL_KEY] = email
         }
     }
 
     suspend fun clearUser() {
         dataStore.edit { preferences ->
             preferences.remove(USER_ID)
-            preferences.remove(EMAIL_KEY)
         }
     }
 
-    fun getUserId() = dataStore.data.map { preference ->
-        preference[USER_ID]
-    }
-
     fun isSessionValid() = dataStore.data.map { preferences ->
-        preferences[USER_ID] != null && preferences[EMAIL_KEY]?.isNotBlank() == true
+        val isValid = preferences[USER_ID] != null
+        Log.d("AuthPreference", "isSessionValid: $isValid")
+        isValid
     }
 
     val userId: Flow<Int?> = dataStore.data.map { preferences ->
         preferences[USER_ID]
-    }
-
-    val email: Flow<String?> = dataStore.data.map { preferences ->
-        preferences[EMAIL_KEY]
     }
 }
