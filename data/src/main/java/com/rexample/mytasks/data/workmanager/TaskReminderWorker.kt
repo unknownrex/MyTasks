5d1f157fb8e1f2d4cbe82eec1aaaba71e50af11f
Rex <reyvan.haltec@gmail.com>
2025-03-17 18:40:38 +0700

    added pending intent to notification

--- data/src/main/java/com/rexample/mytasks/data/workmanager/TaskReminderWorker.kt
+++ data/src/main/java/com/rexample/mytasks/data/workmanager/TaskReminderWorker.kt
@@ -2,7 +2,10 @@ package com.rexample.mytasks.data.workmanager
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.work.Worker
@@ -35,12 +38,20 @@ class TaskReminderWorker(
             notificationManager.createNotificationChannel(channel)
         }
 
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("myapp://taskreminder?taskId=$taskId"))
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext, taskId, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val builder = NotificationCompat.Builder(applicationContext, channelId)
             .setSmallIcon(R.drawable.mytasks_icon)
             .setContentTitle("Pengingat Tugas")
             .setContentText("Tugas \"$taskName\" akan berakhir!")
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
 
         notificationManager.notify(taskId * 1000, builder.build())
     }
